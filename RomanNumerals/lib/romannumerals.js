3f388dc0795d9d8225b523c5343ae722eda55ca6
Pepe García <joseluisgarcia@packlink.es>
2015-02-24 13:07:06 +0100

    Add constant mappings.

--- RomanNumerals/lib/romannumerals.js
+++ RomanNumerals/lib/romannumerals.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var MAPPING = {
+  1: 'I',
+  5: 'V',
+  10: 'X',
+  50: 'L',
+  100: 'C',
+  500: 'D',
+  1000: 'M'
+};
+
 module.exports.convert = function(number) {
-  return 'I';
+  return MAPPING[number];
 };
--- RomanNumerals/test/romannumerals_test.js
+++ RomanNumerals/test/romannumerals_test.js
@@ -5,12 +5,22 @@ var assert = require('should');
 
 describe('Roman numerals', function () {
 
-  it('should return "I" for 1', function () {
-    romannumerals.convert(1).should.equal('I');
-  });
+  var convertsTo = function(dec, rom) {
+    return function() {
+      romannumerals.convert(dec).should.equal(rom);
+    }
+  };
 
-  it('should return "X" for 10', function() {
-    romannumerals.convert(10).should.equal('X');
-  });
-
+  [
+    [1, 'I'],
+    [5, 'V'],
+    [10, 'X'],
+    [50, 'L'],
+    [100, 'C'],
+    [500, 'D'],
+    [1000, 'M']
+        ].forEach(function(pair){
+          it('returns "'+ pair[1] + '" for ' + pair[0],
+             convertsTo(pair[0], pair[1]));
+          })
 });
