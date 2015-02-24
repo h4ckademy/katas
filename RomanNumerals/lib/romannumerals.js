138c36b4d5f9015fc0641e440c08334bfb9b3578
Pepe García <joseluisgarcia@packlink.es>
2015-02-24 12:38:50 +0100

    first commit roman numerals

---
+++ RomanNumerals/lib/romannumerals.js
@@ -0,0 +1,13 @@
+/*
+ * RomanNumerals
+ * https://github.com/pepegar/romannumerals
+ *
+ * Copyright (c) 2015 Pepe
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function() {
+  return 'awesome';
+};
---
+++ RomanNumerals/test/romannumerals_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var romannumerals = require('../');
+var assert = require('should');
+
+describe('romannumerals', function () {
+
+  it('should be awesome', function () {
+    romannumerals().should.equal('awesome');
+  });
+
+});
