60c13475268d3a61e3d70ba8f7487553a49f1554
Pepe García <joseluisgarcia@packlink.es>
2015-02-24 12:49:16 +0100

    created second test

--- RomanNumerals/test/romannumerals_test.js
+++ RomanNumerals/test/romannumerals_test.js
@@ -4,8 +4,13 @@ var romannumerals = require('../');
 var assert = require('should');
 
 describe('Roman numerals', function () {
+
   it('should return "I" for 1', function () {
     romannumerals.convert(1).should.equal('I');
+  });
+
+  it('should return "X" for 10', function() {
+    romannumerals.convert(10).should.equal('X');
   });
 
 });
